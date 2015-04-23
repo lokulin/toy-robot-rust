39b76bd5e2ca67c63e4d2dcf476680e86a58fc0d
lokulin <lauchlin@lauchlin.com>
2015-04-23 16:51:00 +1000

    Working implementation.  Needs some tidying.

--- src/main.rs
+++ src/main.rs
@@ -5,34 +5,61 @@ extern crate regex;
 use std::io::BufRead;
 use std::io::BufReader;
 use std::fs::File;
+use std::env;
 use regex::Regex;
 use toyrobot::*;
 
-fn execute(robot: Robot, line: String) -> Robot {
-    let re = Regex::new(r"^(?P<command>MOVE|LEFT|RIGHT|REPORT|PLACE\s?(\d+),(\d+),(NORTH|EAST|SOUTH|WEST))$").unwrap();
-    let caps = re.captures(line.as_ref()).unwrap();
+fn execute(robot: Robot, line: String, table: Option<Table>, validator: &Regex) -> Robot {
+    let caps = validator.captures(line.as_ref());
 
-    match caps.name("command") {
-        Some("MOVE") => robot.movef(),
-        Some("LEFT") => robot.left(),
-        Some("RIGHT") => robot.right(),
-        Some("REPORT") => {
-            robot.report(); 
-            robot
+    //TODO: This fn could be neater
+    match caps {
+        Some(cmd) => match cmd.name("command") {
+            Some("MOVE") => robot.movef(),
+            Some("LEFT") => robot.left(),
+            Some("RIGHT") => robot.right(),
+            Some("REPORT") => {
+                robot.report();
+                robot
+            },
+            _ => {
+                let x: f64 = cmd.name("x").unwrap().parse().unwrap();
+                let y: f64 = cmd.name("y").unwrap().parse().unwrap();
+                let loc = Point::new(x, y);
+                match cmd.name("dir") {
+                    //TODO: Clean up repetition
+                    Some("NORTH") => robot.place(loc, 0.0, table),
+                    Some("EAST") => robot.place(loc, 0.5, table),
+                    Some("SOUTH") => robot.place(loc, 1.0, table),
+                    Some("WEST") => robot.place(loc, 1.0, table),
+                    _ => robot
+                }
+            }
         },
-        _ => {
-            let re = Regex::new(r"^(?P<place>PLACE)\s?(\d+),(\d+),(NORTH|EAST|SOUTH|WEST)$").unwrap();
-            let caps = re.captures(line.as_ref()).unwrap();
-            robot.place(Point::new(1.0,2.0), 0.5, Some(Table::new(Point::new(0.0,0.0),Point::new(4.0,4.0))))
-        }
+        None => robot
     }
 }
 
 fn main() {
     let robot = Robot::new(Point::new(0.0,0.0), 0.0, None);
+    let table = Some(Table::new(Point::new(0.0,0.0),Point::new(4.0,4.0)));
+    let validator = Regex::new(r"^(?P<command>MOVE|LEFT|RIGHT|REPORT|PLACE\s?(?P<x>\d+),(?P<y>\d+),(?P<dir>NORTH|EAST|SOUTH|WEST))$").unwrap();
 
-    //TODO: Catch file not found etc.
-    BufReader::new(File::open("examples/example5.txt").unwrap())
-                .lines()
-                .fold(robot, |robot, line| execute(robot, line.unwrap()));
+    //TODO: This could be neater
+    if env::args().count() == 2 {
+        match File::open(env::args().nth(1).unwrap()) {
+            Ok(file) => {
+                BufReader::new(file)
+                    .lines()
+                    .fold(robot, |robot, line| execute(robot, line.unwrap(), table, &validator));
+                    ()
+            },
+            _ => {
+                println!("Error reading file: {}", env::args().nth(1).unwrap());
+                ()
+            }
+        }
+    } else {
+        println!("usage: toyrobot <filename>");
+    }
 }
